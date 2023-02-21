a567ac8092e32086d6f91e4e342dd75b6feb4832
Konstantinos Korovesis <kkorovesis88@gmail.com>
2023-02-21 20:32:24 +0200

    - added NER V0.1

--- multilingual_NER.py
+++ multilingual_NER.py
@@ -1,8 +1,14 @@
 import spacy
+from spacy.cli import download
 
 
 def extract_entities(text, lang_code):
-  nlp = spacy.load(f"{lang_code}_core_web_sm")
+  spacy_model_name = f"{lang_code}_core_web_sm"
+  try:
+    nlp = spacy.load(spacy_model_name)
+  except OSError:
+    download(spacy_model_name)
+    nlp = spacy.load(spacy_model_name)
   doc = nlp(text)
   entities = []
   for ent in doc.ents:
@@ -12,5 +18,10 @@ def extract_entities(text, lang_code):
 
 
 if __name__ == '__main__':
+
+  # Run some tests
   text = '''US President Joe Biden declared "Kyiv stands strong" as he marked nearly one year of Russia's invasion of Ukraine with a speech in Poland'''
   print(extract_entities(text, 'en'))
+
+  text = '中华人民共和国是一个伟大的国家。'
+  print(extract_entities(text, 'zh'))
